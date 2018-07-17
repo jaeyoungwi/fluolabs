e40d621a716a4c7fca319812b9e89a49e7f4e8a4
jaeyoungwi <40807196+jaeyoungwi@users.noreply.github.com>
2018-07-16 19:49:45 -0500

    Update fourcircles.py

--- fourcircles.py
+++ fourcircles.py
@@ -10,13 +10,13 @@ def draw():
     global t2, t3, t4
     #circle 1
     x = random(width)
-    ellipse(x, height * 1/5 , 40, 40)
+    ellipse(height * 1/5 , x, 40, 40)
     
     # circle 2
     x = noise(t2)
     x = map( x, 0, 1, 0, width)
     print(x)
-    ellipse(x, height * 2/5, 40, 40)
+    ellipse(height * 2/5, x, 40, 40)
     t2 = t2 + 1
     
     #circle 3
@@ -24,7 +24,7 @@ def draw():
     x = noise(t3)
     x = map( x, 0, 1, 0, width)
     print(x)
-    ellipse(x, height * 3/5, 40, 40)
+    ellipse(height * 3/5, x,  40, 40)
     t3 = t3 + .05
     
     #circle 4
@@ -32,5 +32,5 @@ def draw():
     x = noise(t4)
     x = map( x, 0, 1, 0, width)
     print(x)
-    ellipse(x, height * 4/5, 40, 40)
+    ellipse(height * 4/5, x, 40, 40)
     t4 = t4 + .01
